283c35734b030358782b735d96702baee987acb2
nnovod <nnovod@49feb11a-dc6b-4521-947f-5d0cd0a85b6f>
2015-02-28 15:12:41 +0000

    A bit more transfer stuff
    git-svn-id: https://svn.broadinstitute.org/BTL/tracker@498 49feb11a-dc6b-4521-947f-5d0cd0a85b6f

--- app/controllers/TransferController.scala
+++ app/controllers/TransferController.scala
@@ -4,6 +4,8 @@ import controllers.Errors.FlashingKeys
 import models.Transfer.Quad.Quad
 import models.{ContainerDivisions, Transferrable, Component, Transfer}
 import ContainerDivisions.Division._
+import play.api.Logger
+import play.api.data.Form
 import play.api.mvc.{Action,Controller}
 import play.modules.reactivemongo.MongoController
 import play.modules.reactivemongo.json.collection.JSONCollection
@@ -20,9 +22,15 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 object TransferController extends Controller with MongoController {
 	/**
 	 * Get collection to do mongo operations.  We use a def instead of a val to avoid hot-reloading problems.
-	 * @return collection that uses JSON for input/output
+	 * @return collection that uses JSON for input/output of tracker data
 	 */
-	private def collection: JSONCollection = db.collection[JSONCollection]("tracker")
+	private def trackerCollection: JSONCollection = db.collection[JSONCollection]("tracker")
+
+	/**
+	 * Get collection to do mongo operations.  We use a def instead of a val to avoid hot-reloading problems.
+	 * @return collection that uses JSON for input/output of transfer data
+	 */
+	private def transferCollection: JSONCollection = db.collection[JSONCollection]("transfer")
 
 	/**
 	 * Initiate transfer - go bring up form to do transfer
@@ -62,8 +70,8 @@ object TransferController extends Controller with MongoController {
 	private def getTransferInfo[T: Reads](data: Transfer) = {
 		// Go retrieve both objects via futures
 		for {
-			to <- collection.find(Json.toJson(Json.obj(Component.idKey -> data.to))).one
-			from <- collection.find(Json.toJson(Json.obj(Component.idKey -> data.from))).one
+			to <- trackerCollection.find(Json.toJson(Json.obj(Component.idKey -> data.to))).one
+			from <- trackerCollection.find(Json.toJson(Json.obj(Component.idKey -> data.from))).one
 		} yield {
 			// Method to set form with missing ID(s) - errs is a list of form keys for ID(s) not found
 			def missingIDs(errs: List[String]) = {
@@ -100,27 +108,59 @@ object TransferController extends Controller with MongoController {
 		getComponent(from) match {
 			case fromC: Component with Transferrable if fromC.validTransfers.contains(toC.component) =>
 				(fromC, toC, None)
-			case fromC => {
-				(fromC, toC, Some("Can't do transfer from a " + fromC.component.toString +
-					" to a " + toC.component.toString))
-			}
+			case fromC => (fromC, toC, Some("Can't do transfer from a " + fromC.component.toString +
+				" to a " + toC.component.toString))
 		}
 	}
 
+	/**
+	 * Make message describing component transfer
+ 	 * @param from component we're transferring from
+	 * @param to component we're transferring to
+	 * @return componentType ID to ComponentType ID
+	 */
 	private def fromToMsg(from: Component, to: Component) = {
 		def componentStr(c: Component) = c.component.toString + " " + c.id
 		componentStr(from) + " to " + componentStr(to)
 	}
 
-	private def transferCompleteResponse(getMsg: () => String) =
+	/**
+	 * Result when transfer completes
+	 * @param getMsg callback to get messages to set in response form
+	 * @return redirect to index with wanted message
+	 */
+	private def transferCompleteResult(getMsg: () => String) =
 		FlashingKeys.setFlashingValue(Redirect(routes.Application.index()), FlashingKeys.Status, getMsg())
 
-	private def transferIncompleteResponse(data: Transfer, fromQuad: Boolean, toQuad: Boolean) = {
-		val result0 = Redirect(routes.TransferController.transferWithParams(
+	/**
+	 * Result when transfer needs more information
+	 * @param data transfer info known so far
+	 * @param fromQuad true if need query for quadrant to transfer from
+	 * @param toQuad true if need query for quadrant to transfer to
+	 * @return redirect to transferWithParams to query for additional information
+	 */
+	private def transferIncompleteResult(data: Transfer, fromQuad: Boolean, toQuad: Boolean) = {
+		val result = Redirect(routes.TransferController.transferWithParams(
 			data.from, data.to, Some(fromQuad), Some(toQuad), None, None))
-		FlashingKeys.setFlashingValue(result0,
-			FlashingKeys.Status, "Fill in additional data to complete transfer")
+		FlashingKeys.setFlashingValue(result, FlashingKeys.Status, "Fill in additional data to complete transfer")
 	}
+
+	/**
+	 * Result when transfer form had errors
+	 * @param form form filled with error data
+	 * @return BadRequest to transferStart with input form
+	 */
+	private def transferErrorResult(form: Form[Transfer]) = BadRequest(views.html.transferStart(form))
+
+	/**
+	 * Result when transfer had errors
+	 * @param data data found in transfer form
+	 * @param errs errors to set in form
+	 * @return BadRequest to transferStart with form set with errors
+	 */
+	private def transferErrorResult(data: Transfer, errs: Map[Option[String], String]) =
+		transferErrorResult(Errors.fillAndSetFailureMsgs(errs, Transfer.form, data))
+
 
 	/**
 	 * Start of transfer - we look over IDs and see what's possible.  Depending type of components being transferred
@@ -130,51 +170,41 @@ object TransferController extends Controller with MongoController {
 	def transferIDs = Action.async { request =>
 		Transfer.form.bindFromRequest()(request).fold(
 			formWithErrors =>
-				Future.successful(BadRequest(
-					views.html.transferStart(formWithErrors.withGlobalError(Errors.validationError)))),
+				Future.successful(transferErrorResult(formWithErrors.withGlobalError(Errors.validationError))),
 			data => {
 				// Got data from form - get from and to data (as json) - map is mapping future from retrieving DB data
 				getTransferInfo[JsObject](data).map {
 					// Found both objects - now check if we can transfer between them
 					case (Some((from, to)), None) =>
 						isTransferValid(data, from, to) match {
-							case (fromData, toData, None) if (fromData.id == toData.id) =>
-								BadRequest(views.html.transferStart(
-									Errors.fillAndSetFailureMsgs(Map(None -> "Can not transfer component to itself"),
-										Transfer.form, data)))
-							case (fromData, toData, None) if (fromData.isInstanceOf[ContainerDivisions] &&
-									toData.isInstanceOf[ContainerDivisions]) =>
+							case (fromData, toData, None) if fromData.id == toData.id =>
+								transferErrorResult(data, Map(None -> "Can not transfer component to itself"))
+							case (fromData, toData, None) if fromData.isInstanceOf[ContainerDivisions] &&
+									toData.isInstanceOf[ContainerDivisions] =>
 								(fromData.asInstanceOf[ContainerDivisions].layout,
 									toData.asInstanceOf[ContainerDivisions].layout) match {
-									case (DIM8x12, DIM8x12) | (DIM16x24, DIM16x24) =>
-										transferCompleteResponse(() =>
-											"Transfer completed from " + fromToMsg(fromData, toData))
+									case (DIM8x12, DIM8x12) | (DIM16x24, DIM16x24) => transferCompleteResult(() =>
+										"Transfer completed from " + fromToMsg(fromData, toData))
 									case (DIM8x12, DIM16x24) =>
-										transferIncompleteResponse(data, false, true)
+										transferIncompleteResult(data, fromQuad = false, toQuad = true)
 									case (DIM16x24, DIM8x12) =>
-										transferIncompleteResponse(data, true, false)
+										transferIncompleteResult(data, fromQuad = true, toQuad = false)
 								}
-							case (fromData, toData, None) => transferCompleteResponse(() =>
+							case (fromData, toData, None) => transferCompleteResult(() =>
 								"Transfer completed from " + fromToMsg(fromData, toData))
-							case (fromData, toData, Some(err)) =>
-								BadRequest(views.html.transferStart(
-									Errors.fillAndSetFailureMsgs(Map(None -> err),
-										Transfer.form, data)))
+							case (fromData, toData, Some(err)) => transferErrorResult(data, Map(None -> err))
 						}
 					// Couldn't find one or both data - form returned contains errors - return it now
-					case (None, Some(form)) => BadRequest(views.html.transferStart(form))
+					case (None, Some(form)) => transferErrorResult(form)
 					// Should never have both or neither as None but...
 					case _ => FlashingKeys.setFlashingValue(Redirect(routes.Application.index()),
 						FlashingKeys.Status,"Internal error: Failure during transferIDs")
 				}
 			}.recover {
-				case err => BadRequest(views.html.transferStart(
-					Errors.fillAndSetFailureMsgs(Map(None -> Errors.exceptionMessage(err)), Transfer.form, data)))
-
+				case err => transferErrorResult(data, Map(None -> Errors.exceptionMessage(err)))
 			}
 		).recover {
-			case err => BadRequest(
-				views.html.transferStart(Transfer.form.withGlobalError(Errors.exceptionMessage(err))))
+			case err => transferErrorResult(Transfer.form.withGlobalError(Errors.exceptionMessage(err)))
 		}
 	}
 
@@ -185,19 +215,22 @@ object TransferController extends Controller with MongoController {
 	def transferFromForm = Action.async { request =>
 		Transfer.form.bindFromRequest()(request).fold(
 			formWithErrors =>
-				Future.successful(BadRequest(
-					views.html.transferStart(formWithErrors.withGlobalError(Errors.validationError)))),
+				Future.successful(transferErrorResult(formWithErrors.withGlobalError(Errors.validationError))),
 			data => {
-				Future.successful(transferCompleteResponse(() => {
-					def quadDesc(id: String, quad: Option[Quad]) =
-						(if (quad.isDefined) quad.get.toString + " of " else "") + id
-					"Transfer completed from " + quadDesc(data.from, data.fromQuad) +
-						" to " + quadDesc(data.to, data.toQuad)
-				}))
+				transferCollection.insert(data).map { lastError =>
+					val success = s"Successfully inserted transfer from ${data.from} to ${data.to}"
+					Logger.debug(s"$success with status: $lastError")
+					transferCompleteResult(() => {
+						def quadDesc(id: String, quad: Option[Quad]) =
+							(if (quad.isDefined) quad.get.toString + " of " else "") + id
+						"Transfer completed from " + quadDesc(data.from, data.fromQuad) +
+							" to " + quadDesc(data.to, data.toQuad)	 // Return view of data just created
+					})}
+			}.recover {
+				case err => transferErrorResult(data, Map(None -> Errors.exceptionMessage(err)))
 			}
 		).recover {
-			case err => BadRequest(
-				views.html.transferStart(Transfer.form.withGlobalError(Errors.exceptionMessage(err))))
+			case err => transferErrorResult(Transfer.form.withGlobalError(Errors.exceptionMessage(err)))
 		}
 	}
 
--- app/models/Transfer.scala
+++ app/models/Transfer.scala
@@ -61,31 +61,4 @@ object Transfer {
 			(__ \ fromPosKey).format[Option[String]] ~
 			(__ \ toPosKey).format[Option[String]]
 	  )(Transfer.apply, unlift(Transfer.unapply))
-//	implicit val formatter: Format[Transfer] = Json.format[Transfer] // Not working
-
-	// Rules for a transfer - given two components a rule says that the transfer is (in)valid
-	type TransferRules = (Component,Component) => Boolean
-
-	/**
-	 * Definition for an action
-	 * @param id Identifying ID
-	 * @param from component type being transferred from
-	 * @param to component type being transferred to
-	 * @param rules rules for this type of transfer
-	 */
-	case class ActionDefinition(id: Int, from: ComponentType, to: ComponentType,rules: List[TransferRules])
-
-	/**
-	 * Action that has occured
-	 * @param definition id for action definition
-	 */
-	case class Action(definition: Int)
-
-	/**
-	 * Edge for transfer graph
-	 * @param from ID of component transfer was from
-	 * @param to ID of component transfer was to
-	 * @param actions actions taken during transfer
-	 */
-	case class Edge(from: Int, to: Int, actions: List[Action])
 }
