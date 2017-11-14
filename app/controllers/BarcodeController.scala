ae5ce37bf020db40b042976279dcdeeb2074b7dd
Amr Abouelleil <amr@broadinstitute.org>
2017-11-14 13:19:08 -0500

    BarcodeController stub and route.

--- app/controllers/BarcodeController.scala
+++ app/controllers/BarcodeController.scala
@@ -1,9 +1,12 @@
 package controllers
-import play.api.mvc.{Action, Controller}
+import play.api.mvc._
 
 /**
   * Created by amr on 11/13/2017.
   */
 object BarcodeController extends Controller{
-  // Test comment
+//  // Test comment
+def add(): Action[AnyContent] = Action { request =>
+  Ok("")
+}
 }
