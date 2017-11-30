e5a8ff64e551b7ee7e9913ea3d934432f51da4dd
Amr Abouelleil <amr@dry-kidz.com>
2017-11-30 10:51:00 -0500

    Validation functions: BarcodesFileExtension, BarcodeWell, BarcodeSeq

--- app/validations/BarcodesValidation.scala
+++ app/validations/BarcodesValidation.scala
@@ -6,6 +6,10 @@ package validations
   */
 object BarcodesValidation{
 
+  def validateBarcode(well: String, seq: String): Boolean = {
+    BarcodeWell.isValidWell(well) && BarcodeSeq.isValidSeq(seq)
+  }
+
   object BarcodesFileExtension extends Enumeration {
     type BarcodesFileExtension = Value
     val csv, xls, xlsx = Value
@@ -16,34 +20,41 @@ object BarcodesValidation{
     }
   }
 
+  object Manufacturers extends Enumeration{
+    type manufacturer = Value
+    val Illumina = Value
+
+    def isValidManufacturer(m: String): Boolean = {
+      Manufacturers.values.exists(_.toString == m)
+    }
+  }
+
   object BarcodeWell {
     def isValidRow(r: String): Boolean = {
-      ???
+      if (r.toUpperCase >= "A" && r.toUpperCase <= "P") true
+      else false
     }
 
     def isValidColumn(c: Int): Boolean = {
-      ???
+      if (c >= 1 && c <= 24) true
+      else false
     }
 
     def isValidWell(w: String): Boolean = {
-      ???
+      val row: String = w.replaceAll("[^A-Za-z]", "")
+      val col: Int = w.replaceAll("[^0-9]+", "").toInt
+      isValidRow(row) && isValidColumn(col)
     }
   }
 
   object BarcodeSeq {
     def isValidSeq(s: String): Boolean = {
-      ???
+      s.toUpperCase().matches("[ATCG]+")
+    }
+
+    def isValidLength(s: String): Boolean = {
+      s.length() == 8
     }
   }
-
-
-
-
 }
 
-
-
-object Manufacturers extends Enumeration {
-  type manufacturer = Value
-  val Illumina = Value
-}
--- test/ValidationSpec.scala
+++ test/ValidationSpec.scala
@@ -1,10 +1,11 @@
 import org.scalatest.{FlatSpec, Matchers}
-import validations.BarcodesValidation._
+import validations.BarcodesValidation.{BarcodeSeq, BarcodeWell, BarcodesFileExtension, Manufacturers}
 
 /**
   * Created by Amr on 11/28/2017.
   */
 class ValidationSpec extends FlatSpec with Matchers {
+
   "BarcodeFileExtension.isValidFilename" should "be true when passed filenames with valid extensions" in {
     BarcodesFileExtension.isValidFilename("foo.csv") should be (true)
     BarcodesFileExtension.isValidFilename("/this/is/a/path/foo.xls") should be (true)
@@ -12,7 +13,49 @@ class ValidationSpec extends FlatSpec with Matchers {
   }
   it should "be false when passed filenames with bad extensions" in {
     BarcodesFileExtension.isValidFilename("foo.txt") should be (false)
-    BarcodesFileExtension.isValidFilename("foo.tab") should be (false)
+    BarcodesFileExtension.isValidFilename("/some/path/foo.tab") should be (false)
+    BarcodesFileExtension.isValidFilename("/another/invalid/path/badfile") should be (false)
   }
 
+  "BarcodeWell" should "return true for valid wells and false for invalid ones" in {
+    BarcodeWell.isValidWell("A1") should be (true)
+    BarcodeWell.isValidWell("P09") should be (true)
+    BarcodeWell.isValidWell("G11") should be (true)
+
+  }
+  it should "return false when well row is not A thru P or column is not 1 thru 24" in {
+    BarcodeWell.isValidWell("z06") should be (false)
+    BarcodeWell.isValidWell("A99") should be (false)
+    BarcodeWell.isValidWell("Z99") should be (false)
+  }
+
+  "BarcodeSeq" should "return true if seq only contains DNA bases" in {
+    BarcodeSeq.isValidSeq("ACCTATGC") should be (true)
+    BarcodeSeq.isValidSeq("CtTCTgGC") should be (true)
+    BarcodeSeq.isValidSeq("gatccctt") should be (true)
+
+  }
+  it should "return false if seq contains any non-DNA letters" in {
+    BarcodeSeq.isValidSeq("123456") should be (false)
+    BarcodeSeq.isValidSeq("ACCTxTGC") should be (false)
+    BarcodeSeq.isValidSeq("ACcTnTGC") should be (false)
+  }
+
+  it should "return true if seq is 8 characters long" in {
+    BarcodeSeq.isValidLength("ACCTATGC") should be (true)
+    BarcodeSeq.isValidLength("CtTCTgGC") should be (true)
+    BarcodeSeq.isValidLength("gatccctt") should be (true)
+  }
+  it should "return false if seq is not 8 characters long" in {
+    BarcodeSeq.isValidLength("") should be (false)
+    BarcodeSeq.isValidLength("") should be (false)
+    BarcodeSeq.isValidLength("") should be (false)
+  }
+
+  "Manufacturers" should "be true when passed valid manufacturer name" in {
+    Manufacturers.isValidManufacturer("Illumina")
+  }
+  it should "return false when passed an invalid manufacturer name" in {
+    Manufacturers.isValidManufacturer("Microsoft")
+  }
 }
