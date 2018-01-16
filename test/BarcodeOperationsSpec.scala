d18205fd439083a48e596c8167f373e1b64455af
Amr Abouelleil <amr@broadinstitute.org>
2018-01-16 11:42:31 -0500

    -BarcodeOperationsSpec

--- test/BarcodeOperationsSpec.scala
+++ test/BarcodeOperationsSpec.scala
@@ -1,11 +1,13 @@
 import models.BarcodeSet.BarcodeSet
 import models.initialContents.MolecularBarcodes.MolBarcode
 import org.specs2.mutable._
-import controllers.BarcodesController.{makeBarcodeObjects, makeSetWells, insertBarcodeObjects}
+import controllers.BarcodesController.{insertBarcodeObjects, makeBarcodeObjects, makeSetWells}
+
 import scala.concurrent.Await
 import scala.concurrent.duration.{Duration, SECONDS}
 import play.api.test._
 import play.api.test.Helpers._
+import reactivemongo.bson.BSONDocument
 
 /**
   * Created by amr on 12/4/2017.
@@ -24,12 +26,28 @@ class BarcodeOperationsSpec extends Specification{
   )
 
   // Make our objects from the barcode entries
+  private val setName = "BarcodeOperationsSpec"
   private val barcodeObjects = makeBarcodeObjects(goodEntries)
   private val barcodeWells = makeSetWells(barcodeObjects)
-  private val set = BarcodeSet(name = "BarcodeOperationsSpec", contents = barcodeWells)
+  private val set = BarcodeSet(name = setName, contents = barcodeWells)
+
+  // Credit to Luigi Plinge for sameAs method for testing if two traversables have same contents.
+  // https://stackoverflow.com/questions/7434762/comparing-collection-contents-with-scalatest/24243753
+  def sameAs[A](c: Traversable[A], d: Traversable[A]): Boolean =
+    if (c.isEmpty) d.isEmpty
+    else {
+      val (e, f) = d span (c.head !=)
+      if (f.isEmpty) false else sameAs(c.tail, e ++ f.tail)
+    }
 
   "Good barcodes" should {
     {
+      // Template for unit tests
+//      "" in {
+//        running(TestServer(3333)) {
+//          0 mustEqual 0
+//           }
+//        }
       "be created in DB" in {
         running(TestServer(3333)) {
           //Create Barcodes
@@ -38,8 +56,39 @@ class BarcodeOperationsSpec extends Specification{
           notOk.size mustEqual 0
         }
       }
+      "and have a set created for them" in {
+        running(TestServer(3333)) {
+          val result = Await.result(BarcodeSet.create(set), Duration(5, SECONDS))
+          result.ok mustEqual true
+        }
+      }
+      "and have their barcode IDs contained in the set" in {
+        running(TestServer(3333)) {
+          val setList = Await.result(BarcodeSet.read(BSONDocument("name" -> setName)), Duration(5, SECONDS))
+          //Get data from DB
+          val dbObjectIDs = setList.head.contents.map( w => w.i5Contents.get) ::: setList.head.contents.map(w => w.i7Contents.get)
+          // Get original data
+          val originalIDs = barcodeObjects.map(o => {
+            o.get._2 match {
+              case Left(l) =>
+                val b = Await.result(MolBarcode.read(BSONDocument("seq" -> l.seq)), Duration(5, SECONDS))
+                (b.head._id, b.head._id)
+              case Right(r) =>
+                val b1 = Await.result(MolBarcode.read(BSONDocument("seq" -> r.i7.seq)), Duration(5, SECONDS))
+                val b2 = Await.result(MolBarcode.read(BSONDocument("seq" -> r.i5.seq)), Duration(5, SECONDS))
+                (b1.head._id, b2.head._id)
+            }
+          })
+          val originalIDsList = originalIDs.map(_._1) ++ originalIDs.map(_._2)
+          // The object IDs we get from mongodb must equal the IDs contained in the barcode objects.
+          sameAs(dbObjectIDs, originalIDsList) mustEqual true
+
+          //A negative test to make sure sameAs will return false when two lists don't match
+          sameAs(dbObjectIDs, List(1,2,3)) mustEqual false
+          }
+        }
       // This should always be last test
-      "be deleted from DB" in {
+      "and finally be deleted from DB." in {
         running(TestServer(3333)) {
           val result2 = barcodeObjects.map(b => {
             b.get._2 match {
@@ -51,6 +100,9 @@ class BarcodeOperationsSpec extends Specification{
           })
           val notOk2 = result2.filter(futureLE => !Await.result(futureLE, Duration(5, SECONDS)).ok)
           notOk2.size mustEqual 0
+
+          //Delete and test deletion of BarcodeSet
+          Await.result(BarcodeSet.delete(set), Duration(5, SECONDS)).ok mustEqual true
         }
       }
     }
