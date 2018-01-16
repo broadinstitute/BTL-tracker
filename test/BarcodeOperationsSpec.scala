539b40e6b2565509f4be17af4d41245e8e306ffe
Amr Abouelleil <amr@broadinstitute.org>
2018-01-16 08:48:59 -0500

    rolled back to working unit test

--- test/BarcodeOperationsSpec.scala
+++ test/BarcodeOperationsSpec.scala
@@ -1,12 +1,13 @@
-import models.BarcodeSet.BarcodeSet
-import controllers.BarcodesController.{makeBarcodeObjects, makeSetWells, insertBarcodeObjects}
-import models.initialContents.MolecularBarcodes.{MolBarcode, MolBarcodeNexteraPair}
+import models.initialContents.MolecularBarcodes.MolBarcode
+import org.scalatest.{FlatSpec, Matchers}
 import org.specs2.mutable._
+
+import scala.concurrent.duration.{Duration, SECONDS}
+import scala.concurrent.{Await, Future}
 import play.api.test._
 import play.api.test.Helpers._
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 
+import scala.util.{Failure, Success}
 
 
 /**
@@ -14,37 +15,34 @@ import scala.concurrent.duration.Duration
   * A set of tests for db operations related to barcodes.
   */
 class BarcodeOperationsSpec extends Specification{
-  //This forces tests to be run sequentially which is what I want in this case.
-  override def is = args(sequential = true) ^ super.is
   // set up a bunch of molbarcode objects.
-  private val goodEntries = List(
-    Map("Well" -> "A1", "Row" -> "A", "Column" -> "01", "P7 Index" -> "AAGTAGAG", "P5 Index" -> "ATCGACTG", "name" -> "Illumina_P5-Feney_P7-Biwid"),
-    Map("Well" -> "b1", "Row" -> "B", "Column" -> "01", "P7 Index" -> "ggtccaga", "P5 Index" -> "GCTAGCAG", "name" -> "Illumina_P5-Poded_P7-Rojan"),
-    Map("Well" -> "C01", "Row" -> "C", "Column" -> "01", "P7 Index" -> "GCACATCT", "P5 Index" -> "TaCtCTcC", "name" -> "Illumina_P5-Wexoj_P7-Pahol")
+  private val goodBarcodes = List(
+    MolBarcode("AAGTAGAG", "Biwid"),
+    MolBarcode("ggtccaga", "Rojan"),
+    MolBarcode("GCACATCT", "Pahol"),
+    MolBarcode("TTCGCTGA", "Zepon"),
+    MolBarcode("AGCAATTC", "Debox"),
+    MolBarcode("CACATCCT", "Hefel"),
+    MolBarcode("CCAGTTAG", "Jatod"),
+    MolBarcode("AAGGATGT", "Binot"),
+    MolBarcode("ACACGATC", "Cakax"),
+    MolBarcode("CATGCTTA", "Hopow")
   )
-  // Make our objects from the barcode entries
-  private val barcodeObjects = makeBarcodeObjects(goodEntries)
-  private val barcodeWells = makeSetWells(barcodeObjects)
-  private val set = BarcodeSet(name = "BarcodeOperationsSpec", contents = barcodeWells)
-  //TODO: This running testserver business is super buggy. Half the time i get 'application not running' error. This makes testing DB operations very frustrating.
-  running(TestServer(3333)) {
-    // When setwells are created they are also put in the DB.
-    "Good barcodes" should {
-      {
-        "be created in a local database" in {
-          val result = insertBarcodeObjects(barcodeObjects)
-          0 shouldEqual 0
+
+  "Good barcodes" should {
+
+    {
+
+      "be added to the database" in {
+        running(TestServer(3333)) {
+          val response = goodBarcodes.map(b => MolBarcode.create(b))
+          0 mustEqual 0
         }
-//        "be deleted from the local database" in {
-//          barcodeObjects.map(o => o.get._2 match {
-//            case Left(i7) => MolBarcode.delete(i7)
-//            case Right(pair) =>
-//              MolBarcode.delete(pair.i5)
-//              MolBarcode.delete(pair.i7)
-//          })
-//        0 shouldEqual 0
-//        }
       }
     }
+
   }
+  // do the various db operations on them
+  // test that what was expected happened.
+
 }
